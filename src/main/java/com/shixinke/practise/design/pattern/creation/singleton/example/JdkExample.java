a9242273c5cb606f0e779437ab5490a77854586d
shixinke <ishixinke@qq.com>
2019-09-20 18:13:21 +0800

    添加源码分析

---
+++ src/main/java/com/shixinke/practise/design/pattern/creation/singleton/example/JdkExample.java
@@ -0,0 +1,16 @@
+package com.shixinke.practise.design.pattern.creation.singleton.example;
+
+/**
+ * jdk中的单例模式
+ * @author shixinke
+ */
+public class JdkExample {
+
+    public static void main(String[] args) {
+        /**
+         * 懒汉式
+         */
+        Runtime runtime = Runtime.getRuntime();
+        System.out.println(runtime.availableProcessors());
+    }
+}
