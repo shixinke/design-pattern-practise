b681f9133415b963117aa7ceff202498ac96fc72
jiangfangtao <jiangfangtao@qq.com>
2019-10-06 11:10:46 +0800

    更新ignore

--- src/main/java/com/shixinke/practise/design/pattern/content/creation/factory/factorymethod/example/LoggerTest.java
+++ src/main/java/com/shixinke/practise/design/pattern/content/creation/factory/factorymethod/example/LoggerTest.java
@@ -1,9 +1,9 @@
 package com.shixinke.practise.design.pattern.content.creation.factory.factorymethod.example;
 
-import org.slf4j.LoggerFactory;
+//import org.slf4j.LoggerFactory;
 
 public class LoggerTest {
     public static void main(String[] args) {
-        LoggerFactory.getLogger(LoggerTest.class);
+        //LoggerFactory.getLogger(LoggerTest.class);
     }
 }
